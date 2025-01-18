7b95c5651bdbd2183eb275d69ed54cb1f76d467f
Francesco Bruzzesi <42817048+FBruzzesi@users.noreply.github.com>
2025-01-18 12:44:45 +0100

    feat: minimal working example (#5)

--- tests/test_pydantic/datetime_test.py
+++ tests/test_pydantic/datetime_test.py
@@ -4,6 +4,7 @@ from datetime import datetime  # noqa: TC003
 from typing import Optional
 
 import narwhals as nw
+import pytest
 from pydantic import AwareDatetime
 from pydantic import BaseModel
 from pydantic import FutureDatetime
@@ -11,6 +12,7 @@ from pydantic import NaiveDatetime
 from pydantic import PastDatetime
 
 from anyschema._pydantic import model_to_nw_schema
+from anyschema.exceptions import UnsupportedDTypeError
 
 
 def test_parse_datetime() -> None:
@@ -20,12 +22,6 @@ def test_parse_datetime() -> None:
         py_dt_optional: Optional[datetime]  # noqa: UP007
         py_dt_or_none: datetime | None
         none_or_py_dt: None | datetime
-
-        # pydantic AwareDatetime type
-        aware_dt: AwareDatetime
-        aware_dt_optional: Optional[AwareDatetime]  # noqa: UP007
-        aware_dt_or_none: AwareDatetime | None
-        none_or_aware_dt: None | AwareDatetime
 
         # python NaiveDatetime type
         naive_dt: NaiveDatetime
@@ -48,3 +44,29 @@ def test_parse_datetime() -> None:
     schema = model_to_nw_schema(DatetimeModel)
 
     assert all(value == nw.Datetime() for value in schema.values())
+
+
+def test_raise_aware_datetime() -> None:
+    class AwareDatetimeModel(BaseModel):
+        aware_dt: AwareDatetime
+
+    class OptAwareDatetimeModel(BaseModel):
+        aware_dt: Optional[AwareDatetime]  # noqa: UP007
+
+    class AwareDatetimeOrNoneModel(BaseModel):
+        aware_dt: AwareDatetime | None
+
+    class NoneOrAwareDatetimeModel(BaseModel):
+        aware_dt: None | AwareDatetime
+
+    with pytest.raises(UnsupportedDTypeError, match="pydantic AwareDatetime does not specify a fixed timezone."):
+        model_to_nw_schema(AwareDatetimeModel)
+
+    with pytest.raises(UnsupportedDTypeError, match="pydantic AwareDatetime does not specify a fixed timezone."):
+        model_to_nw_schema(OptAwareDatetimeModel)
+
+    with pytest.raises(UnsupportedDTypeError, match="pydantic AwareDatetime does not specify a fixed timezone."):
+        model_to_nw_schema(AwareDatetimeOrNoneModel)
+
+    with pytest.raises(UnsupportedDTypeError, match="pydantic AwareDatetime does not specify a fixed timezone."):
+        model_to_nw_schema(NoneOrAwareDatetimeModel)
